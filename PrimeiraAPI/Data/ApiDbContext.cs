437e0db86fa722ff21f3187e100ea2216465c849
Marcelo Amador <marcelo@gmail.com>
2023-10-26 17:21:09 -0300

    Configuring EF

---
+++ PrimeiraAPI/Data/ApiDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PrimeiraAPI.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
